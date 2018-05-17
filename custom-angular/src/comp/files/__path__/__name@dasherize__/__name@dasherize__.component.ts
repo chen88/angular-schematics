4973c81420eedd64fc8d4730198530b16ebfedad
System Administrator <chen112788@gmail.com>
2018-05-16 23:44:14 -0500

    updated component service injection

--- custom-angular/src/comp/files/__path__/__name@dasherize__/__name@dasherize__.component.ts
+++ custom-angular/src/comp/files/__path__/__name@dasherize__/__name@dasherize__.component.ts
@@ -5,6 +5,7 @@ import { <%= classify(name) %>Service } from './<%= dasherize(name) %>.service'
   selector: '<%= dasherize(name) %>',
   template: require('./<%= dasherize(name) %>.component.pug'),
   styleUrls: ['./<%= dasherize(name) %>.component.scss'],
+  providers: [<%= classify(name) %>Service]
 })
 export class <%= classify(name) %>Component implements OnInit {
   constructor (private <%= camelize(name) %>Service: <%= classify(name) %>Service) {}
