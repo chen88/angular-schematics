6ac7378e9c1df713cb4276af8268fba40b378b77
System Administrator <chen112788@gmail.com>
2018-05-19 01:03:45 -0500

    fixed directive

--- custom-angular/src/directive/files/__path__/directives/__name@dasherize__/__name@dasherize__.directive.ts
+++ custom-angular/src/directive/files/__path__/directives/__name@dasherize__/__name@dasherize__.directive.ts
@@ -1,7 +1,7 @@
 import { Directive } from '@angular/core'
 
 @Directive({
-  selector: '[<%= name %>Dir]'
+  selector: '[<%= camelize(name) %>Dir]'
 })
 export class <%= classify(name) %>Directive {
   constructor () {}
